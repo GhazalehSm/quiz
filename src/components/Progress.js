ac4d733a68a19c69b8aed11b6576d42626466830
GhazalehSm <ghazaleh.samimi91@gmail.com>
2026-09-18 19:03:44 +0200

    implementation

---
+++ src/components/Progress.js
@@ -0,0 +1,25 @@
+function Progress({
+  questionIndex,
+  numQuestions,
+  points,
+  maxPossiblePoints,
+  answer,
+}) {
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestions}
+        value={questionIndex + Number(answer !== null)}
+      />
+      <p>
+        Question <strong>{questionIndex + 1}</strong> / {numQuestions}
+      </p>
+
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
